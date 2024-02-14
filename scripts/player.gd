efc846cedd175cd9337adeb6a9aa5635df756210
Tony Cha <tony.cha@gmail.com>
2024-02-14 16:16:15 -0500

    add inventory popup

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+signal water_entered
+
 const GRAVITY = 200.0
 const WALK_SPEED = 200
 const WALK_SPEED_MULTIPLIER = 4
@@ -30,27 +32,53 @@ var fish_names = ["Atlantic Bass",
 var sprites = []
 var fish_inventory = []
 var num_caught = 0
+const MAX_FISH = 8
+const MAX_FISH_TYPE = 16
 
 func _init():
-	for i in range(16):
+	for i in range(MAX_FISH_TYPE):
 		var fish_png = "res://assets/fish/fish%03d.png" % i
 		print(fish_png)
 		sprites.append(load(fish_png))
+
+func get_fish_inv_coordinate(num_caught):
+	const HORIZ_LIMIT = 4
+	const VERT_LIMIT = 2
+	
+	var x = (num_caught - 1) % HORIZ_LIMIT
+	var y = (num_caught - 1) / HORIZ_LIMIT
+	if (y > VERT_LIMIT):
+		y = VERT_LIMIT-1;
+	return Vector2(x, y)
 	
 func _process(delta):
+	if Input.is_key_label_pressed(KEY_I):
+		$Chest.visible = true
+		
+	if Input.is_key_label_pressed(KEY_ESCAPE):
+		$Chest.visible = false
+		
 	if Input.is_action_just_pressed("ui_accept"):
 		num_caught += 1
-		if (num_caught > 15):
+		if (num_caught > MAX_FISH):
 			return
 		var i = rng.randi_range(0,15)
 		print(fish_names[i])
 		var my_sprite = Sprite2D.new()
 		my_sprite.texture = sprites[i] 
-		my_sprite.scale = Vector2(1,1)
-		my_sprite.offset = Vector2(-20,16*(num_caught+1))
-		add_child(my_sprite)
+		my_sprite.scale = Vector2(1.5,1.5)
+		#my_sprite.offset = Vector2(-20,16*(num_caught+1))
+		var v = get_fish_inv_coordinate(num_caught)
+		v *= 32
+		my_sprite.offset = v + Vector2(-52,-82)
+		$Chest.add_child(my_sprite)
 		
 func _physics_process(delta):
 	velocity.x = Input.get_axis("ui_left", "ui_right") * WALK_SPEED * WALK_SPEED_MULTIPLIER
 	velocity.y = Input.get_axis("ui_up", "ui_down") * WALK_SPEED * WALK_SPEED_MULTIPLIER
 	move_and_slide()
+
+
+func _on_water_entered():
+	print("water entered")
+	pass # Replace with function body.
